165173317e4eb385de15e70168030214eb89dda5
Dick Brouwers <dick@insided.nl>
2020-05-05 11:05:50 +0200

    distances

---
+++ distance.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type world struct {
+	radius float64
+}
+
+type location struct {
+	lat  float64
+	long float64
+}
+
+type coordinate struct {
+	d, m, s float64
+	h       rune
+}
+
+func (c coordinate) decimal() float64 {
+	sign := 1.0
+	switch c.h {
+	case 'S', 'W', 's', 'w':
+		sign = -1
+	}
+	return sign * (c.d + c.m/60 + c.s/3600)
+}
+
+func (w world) distance(p1, p2 location) float64 {
+	s1, c1 := math.Sincos(rad(p1.lat))
+	s2, c2 := math.Sincos(rad(p2.lat))
+	clong := math.Cos(rad(p1.long - p2.long))
+
+	return w.radius * math.Acos(s1*s2+c1*c2*clong)
+}
+
+func rad(deg float64) float64 {
+	return deg * math.Pi / 180
+}
+
+func newWorld(radius float64) world {
+	return world{radius}
+}
+
+func newLocation(lat, long coordinate) location {
+	return location{lat.decimal(), long.decimal()}
+}
+
+func main() {
+	mars := newWorld(3389.5)
+	mountSharp := newLocation(
+		coordinate{5.0, 4.0, 48.0, 'S'},
+		coordinate{137.0, 51.0, 0.0, 'E'})
+	olympusMons := newLocation(
+		coordinate{18.0, 39.0, 0.0, 'N'},
+		coordinate{226.0, 12.0, 0.0, 'E'})
+
+	fmt.Printf("%.2f", mars.distance(mountSharp, olympusMons))
+}
